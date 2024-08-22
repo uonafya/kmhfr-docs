ff7b09a860e4f52f98f55e655a3c8f168d66f2d9
eric <erotush77@gmail.com>
2024-08-22 18:46:40 +0300

    Updated docusaurus.config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://kmhfr-docs.github.io',
+  url: 'https://uonafya.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ertush', // Usually your GitHub org/user name.
+  organizationName: 'uonafya', // Usually your GitHub org/user name.
   projectName: 'kmhfr-docs', // Usually your repo name.
   deploymentBranch:'master',
   onBrokenLinks: 'throw', 
