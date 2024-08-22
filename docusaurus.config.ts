4b9bc48ea9a01c7be3ce5ada1f4d83c0259b0ff2
eric <erotush77@gmail.com>
2024-08-22 19:08:48 +0300

    Changed deployment branch to master

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'uonafya', // Usually your GitHub org/user name.
   projectName: 'kmhfr-docs', // Usually your repo name.
-  deploymentBranch:'main',
+  deploymentBranch:'master',
   onBrokenLinks: 'throw', 
   trailingSlash: false,
   onBrokenMarkdownLinks: 'warn',
